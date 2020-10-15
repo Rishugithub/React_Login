7ef65a8312346ab9f7bfc8a9ba0f481699d75700
Rishugithub <53018657+Rishugithub@users.noreply.github.com>
2020-10-15 13:34:01 +0530

    login and registration page
    implemented login and registration page and validation  added when userid an password is same

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# React_Login
-design login page
+A simple React Login/Register Form
+
+Used create-react-app to bootstrap the project
+Git clone the repo
+Do NPM Install to install all packages
+npm start will start the project
---
+++ src/components/Login.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Login extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			email: '',
+			password: '',
+			loginvalidation:false
+		};
+
+		this.update = this.update.bind(this);
+
+		this.displayLogin = this.displayLogin.bind(this);
+	}
+
+	update(e) {
+		let name = e.target.name;
+		let value = e.target.value;
+		this.setState({
+			[name]: value,
+			
+		});
+	}
+
+	displayLogin(e) {
+		e.preventDefault();
+		console.log('You are logged in');
+		console.log("email and assword"+this.state.email+this.state.password);
+		if(this.state.email===this.state.password){
+			this.setState({
+				loginvalidation:true
+			})
+		} 
+		console.log(this.state);
+		this.setState({
+			email: '',
+			password: ''
+		});
+	}
+
+	render() {
+		
+		return (
+			<div className="login">
+				<form onSubmit={this.displayLogin}>
+					<h2>Login</h2>
+					<div className="username">
+						<input
+							type="text"
+							placeholder="Username..."
+							value={this.state.email}
+							onChange={this.update}
+							name="email"
+						/>
+					</div>
+
+					<div className="password">
+						<input
+							type="password"
+							placeholder="Password..."
+							value={this.state.password}
+							onChange={this.update}
+							name="password"
+						/>
+					</div>
+
+					<input type="submit" onClick={this.validateLogin} value="Login" />
+				</form>
+			
+				<Link to="/register">Create an account</Link>
+			</div>
+		);
+	}
+}
+
+export default Login;
